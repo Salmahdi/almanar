0a31b476e6d89150b53d5db7db78ef73154576c4
Salmahdi <salemalmahdi71@gmail.com>
2023-11-26 14:40:04 +0300

    first version

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+
+const elements = document.querySelectorAll(".nav-item");
+
+elements.forEach((element, index) => {
+  console.log(element);
+  element.addEventListener("click", function () {
+  document.querySelector(".active")?.classList.remove("active");
+  element.classList.add("active");
+  });
+  
+});
+
+
+
+//Counter
+let nums = document.querySelectorAll(".nums .num");
+let section = document.getElementById("about");
+let started = false;
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+console.log(document.getElementById("about").offsetTop);
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop / 2) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
